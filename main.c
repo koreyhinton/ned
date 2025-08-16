10bf84643cd13d9fa4e615cc3338f0212d8da28a
Korey Hinton <2801399+koreyhinton@users.noreply.github.com>
2025-08-16 08:06:50 -0400

    syntax error unit tests (fail status until all impl.)

--- main.c
+++ main.c
@@ -2,6 +2,45 @@
 #include <stdlib.h>
 #include "token.h"
 
+char *map(int i) {
+    switch(i) {
+        case 0:
+            return "TOKEN_MODULE_IDENTIFIER";
+        case 1:
+            return "TOKEN_VARIABLE_IDENTIFIER";
+        case 2:
+            return "TOKEN_COLON";
+        case 3:
+            return "TOKEN_SPACE";
+        case 4:
+            return "TOKEN_INDENT";
+        case 5:
+            return "TOKEN_NEWLINE";
+        case 6:
+            return "TOKEN_0";
+        case 7:
+            return "TOKEN_1";
+        case 8:
+            return "TOKEN_STRING";
+        case 9:
+            return "TOKEN_NUMBER";
+        case 10:
+            return "TOKEN_NEGATE";
+        case 11:
+            return "TOKEN_TYPE_MONAD";
+        case 12:
+            return "TOKEN_DOT";
+        case 13:
+            return "TOKEN_AS";
+        case 14:
+            return "TOKEN_LOOP";
+        case 15:
+            return "TOKEN_EQUALS";
+        default:
+            return "tbd";
+    }
+}
+
 int main(int argc, char *argv[]) {
     // printf("%d\n", argc);
     // printf("%s\n", argv[1]);
@@ -28,8 +67,14 @@ int main(int argc, char *argv[]) {
 
     for (int i = 0; tokens[i].type != TOKEN_EOP; i++)
     {
+        /*
         fprintf(stderr, "%d", i);
         fflush(stderr);
+        */
+        
+        fprintf(stderr, "%s(%s)\n", map(tokens[i].type), tokens[i].lexeme);
+        fflush(stderr);
+
 
         if (tokens[i].type == TOKEN_INVALID)
         {
@@ -41,8 +86,10 @@ int main(int argc, char *argv[]) {
         else
         {
             // debug to stderr for now
+            /*
             fprintf(stderr, "%s", tokens[i].lexeme ? tokens[i].lexeme : "(null)");
             fflush(stderr);
+            */
         }
         // printf("token: %s \n", tokens[i].lexeme);
         free(tokens[i].lexeme);
